1a1ce2dbb934ed23cd236564772b600a82b3b377
Tanner C <tzc005@yahoo.com>
2020-10-29 17:44:11 -0700

    just need to display image instead of string

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
-var answers = ["It is certain", 
-                   "It is decidedly so", 
-                   "Without a doubt", 
-                   "Yes - definitely",
-                   "You may rely on it", 
-                   "As I see it, yes", 
-                   "Most likely", 
-                   "Outlook good", 
-                   "Yes", "Signs point to yes",
-                   "Don't count on it", 
-                   "My reply is no",
-                   "My sources say no", 
-                   "Outlook not so good",
-                   "Very doubtful", 
-                   "Reply hazy, try again", 
-                   "Ask again later", 
-                   "Better not tell you now",
-                   "Cannot predict now", 
-                   "Concentrate and ask again"];
+var answers = [
+  '.img/magic8ball_1.png',
+  '.img/magic8ball_2.png',
+  '.img/magic8ball_3.png',
+  '.img/magic8ball_4.png',
+  '.img/magic8ball_5.png',
+  '.img/magic8ball_6.png',
+  '.img/magic8ball_7.png',
+  '.img/magic8ball_8.png',
+  '.img/magic8ball_9.png',
+  '.img/magic8ball_10.png',
+  '.img/magic8ball_11.png',
+  '.img/magic8ball_12.png',
+  '.img/magic8ball_13.png',
+  '.img/magic8ball_14.png',
+  '.img/magic8ball_15.png',
+  '.img/magic8ball_16.png',
+  '.img/magic8ball_17.png',
+  '.img/magic8ball_18.png',
+  '.img/magic8ball_19.png',
+  '.img/magic8ball_20.png',
+
+];
     
     window.onload = function() {
-       var eight = document.getElementById("eight");
        var answer = document.getElementById("answer");
        var eightball = document.getElementById("eight-ball");
        var question = document.getElementById("question");
        
-       eightball.addEventListener("click", function() {
+       ask.addEventListener("click", function() {
          if (question.value.length < 1) {
-           alert('Enter a question!');
+           alert('Please enter a question');
          } else {
-           eight.innerText = "";
            var num = Math.floor(Math.random() * Math.floor(answers.length));
-           answer.innerText = answers[num];
+           answer.innerHTML = answers[num];
+           
          }
        });
     };
